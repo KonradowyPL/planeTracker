4df1837be69ea3f721777c9bac9241e2d4388f5a
KonradowyPL <106955367+KonradowyPL@users.noreply.github.com>
2026-05-13 12:42:28 +0200

    continue webhook refactor

--- src/webhook.py
+++ src/webhook.py
@@ -23,16 +23,19 @@ b = builder
 
 def getter(data):
     def get(*path):
-        nonlocal data
+        current = data
 
         for key in path:
-            if (isinstance(data, dict) and key in data) or (
-                isinstance(data, list) and isinstance(key, int) and len(data) > key
+            if (isinstance(current, dict) and key in current) or (
+                isinstance(current, list)
+                and isinstance(key, int)
+                and len(current) > key
             ):
-                data = data[key]
+                current = current[key]
             else:
                 return None
-        return data
+
+        return current
 
     return get
 
@@ -40,17 +43,12 @@ def getter(data):
 class Message:
     delta = 0
     webhookUrl = config["webhook"]
-    launches = 0
-    landings = 0
     embeds = []
     skipped = []
-    files = {}
+    files = []
 
     def __init__(self, delta):
         self.delta = delta
-
-    def addSkipped(self, text):
-        self.skipped.append(text)
 
     def addEmbed(self, flight):
         get = getter(flight)
@@ -58,22 +56,19 @@ class Message:
         trace, feedback = makeTrace(flight["track"])
 
         if trace is None:
-            _from = (get("airport", "origin", "name") or "N/A") + b(
+            origin = (get("airport", "origin", "name") or "N/A") + b(
                 "  (<t:{}:t>)", get("time", "real", "departure"), ""
             )
-            to = (get("airport", "destination", "name") or "N/A") + b(
+            destination = (get("airport", "destination", "name") or "N/A") + b(
                 "  (<t:{}:t>)", get("time", "real", "arrival"), ""
             )
-            self.addSkipped(
-                f"[{get('aircraft','identification', 'registration') or '??' }](https://www.flightradar24.com/data/aircraft/{get('identification','callsign')}#{get('identification','id')}) from: {_from} to: {to}"
+            self.skipped.append(
+                f"[{get('aircraft','identification', 'registration') or '??' }](https://www.flightradar24.com/data/aircraft/{get('identification','callsign')}#{get('identification','id')}) from: {origin} to: {destination}"
             )
             return
 
-        self.files[flightId] = (
-            f"{flightId}.webp",
-            trace,
-            "image/webp",
-        )
+        self.files.append(trace)
+
         self.embeds.append(
             {
                 "title": f"✈️ FLight: {get('aircraft','identification', 'registration') or '??' }",
@@ -105,72 +100,29 @@ class Message:
         )
 
     def sendMessage(self):
+        # empty
         if len(self.embeds) + len(self.skipped) == 0:
-            return requests.post(webhookUrl, data={"content": "No flights today :("})
+            response = requests.post(
+                webhookUrl, data={"content": "No flights today :("}
+            )
+            response.raise_for_status()
 
-        message = ""
+        content = ""
 
         if self.delta != 0:
-            message = f"**This report is based on flight data from {-self.delta} day(s) ago**\n"
+            content = f"**This report is based on flight data from {-self.delta} day(s) ago**\n"
 
+        # skipped flights
         if len(self.skipped) > 0:
-            message += f"{len(self.skipped)} self.skipped flights.\n"
-            message += "\n".join(self.skipped)
+            content += f"{len(self.skipped)} skipped flights:\n"
+            content += "\n".join(self.skipped)
 
-        if len(self.embeds) == 0:
-            payload_json = json.dumps(
-                {
-                    "content": message,
-                    "tts": False,
-                    "username": config.get("name"),
-                    "icon": config.get("icon"),
-                }
-            )
-
-            response = requests.post(webhookUrl, data={"payload_json": payload_json})
-            response.raise_for_status()
-            return
-
-        message += (
+        content += (
             f"\n{len(self.embeds)} flight{'s' if len(self.embeds) > 1 else ''} today:"
         )
 
-        for index in range(0, len(self.embeds), 10):
-            msgembeds = self.embeds[index : (index + 10)]
-            embed_img_ids = set(
-                embed["image"]["url"].split("attachment://")[1].replace(".webp", "")
-                for embed in msgembeds
-            )
-            filtered_files = {
-                imgId: file_data
-                for imgId, file_data in self.files.items()
-                if imgId in embed_img_ids
-            }
+        for page in range(0, len(self.embeds), 10):
+            self.sendPage(page, content if page == 0 else "")
 
-            print(
-                "message with:",
-                len(msgembeds),
-                "self.embeds,",
-                len(filtered_files),
-                "files",
-            )
-
-            payload_json = json.dumps(
-                {
-                    "content": message[:1999],
-                    "tts": False,
-                    "username": config.get("name"),
-                    "embeds": msgembeds,
-                    "icon": config.get("icon"),
-                }
-            )
-
-            message = ""
-
-            response = requests.post(
-                webhookUrl, files=filtered_files, data={"payload_json": payload_json}
-            )
-
-            response.raise_for_status()
-
-            print("\n", response.text)
+    def sendPage(self, page: int, content: str):
+        print("sending page", page, "with content", content)
